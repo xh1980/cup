cfa5323de4494bac4d022ee2aedf421444d83bec
邢　浩 <keikou@e-business.co.jp>
2026-01-29 10:50:17 +0900

    modify J001 log

--- src/main/java/com/embrace/cup/job/J001.java
+++ src/main/java/com/embrace/cup/job/J001.java
@@ -6,6 +6,6 @@ import com.embrace.cup.zoo.JobExecutor;
 public class J001 implements JobExecutor {
 
     public void execute(String[] params) {
-        Log.info(J001.class.getSimpleName(), String.join(",", params) );
+        Log.info(J001.class.getSimpleName(), "params:" + String.join(",", params) );
     }
 }
